67bd5bffe0395ef9cb314a0b7ce600556b412a35
Mariyappa Ayyakkannu <mariyappa.a@gmail.com>
2024-12-16 07:59:02 +0530

    Callable Merge Sort

---
+++ src/threadedMegerSort/Sorter.java
@@ -0,0 +1,14 @@
+package threadedMegerSort;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class Sorter implements Callable<List<Integer>>{
+
+	@Override
+	public List<Integer> call() throws Exception {
+		// TODO Auto-generated method stub
+		return List.of();
+	}
+	
+}
